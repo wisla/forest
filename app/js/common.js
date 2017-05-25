9cc75ce100d65c6b71d526ebe0f5de937dfe0e0c
andrzej.wislowski@combeenut.com <Sukces2015!>
2017-05-25 22:10:44 +0200

    front

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,20 @@
 $(function() {
 
-	// Custom JS
+	$('#my-menu').mmenu({
+		extensions: ['theme-black', 'effect-menu-slide', 'pagedim-black'],
+		navbar: {
+			title: '<img src="img/logo-1.svg" alt="Salon Beauty">'
+		},
+		offCanvas: {
+			position: 'right'
+		}
+	});
+
+	var api = $("#my-menu").data( "mmenu" );
+	api.bind( "open:finish", function() {
+		$('.hamburger').addClass('is-active');
+	}).bind( "close:finish", function() {
+		$('.hamburger').removeClass('is-active');
+	});
 
 });
